de0d81a68afd9247aa534d28c8b8efd6181c3c74
Jay12473 <160490373+Jay12473@users.noreply.github.com>
2024-03-11 21:09:30 +0800

    Create bard.js

---
+++ scripts/cmds/bard.js
@@ -0,0 +1,103 @@
+const axios = require('axios');
+
+const geminiApiKey = "AIzaSyAbjU4jXdSrOidULKwAUPloYzgi8kGuUUM";
+
+const chatbot = {
+  config: {
+    name: "crowx",
+    version: "2.0",
+    author: "Cruizex",
+    description: "Generate Responses using Gemini | Vertex by Google LLMs",
+  },
+
+  async makeGeminiApiRequest(userInput) {
+    try {
+      const response = await axios.post(
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent",
+        {
+          contents: [{
+            parts: [{
+              text: userInput
+            }]
+          }],
+        },
+        {
+          params: { key: geminiApiKey },
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      const generatedText = response.data.candidates[0].content.parts[0].text;
+      return generatedText;
+    } catch (error) {
+      console.error("Error making Gemini API request:", error.message);
+      throw error;
+    }
+  },
+
+  async handleCommand({ message, event, args, api }) {
+    try {
+      const uid = event.senderID;
+      const encodedPrompt = encodeURIComponent(args.join(" "));
+
+      if (!encodedPrompt) {
+        return message.reply("Please provide questions");
+      }
+
+      const geminiResponse = await this.makeGeminiApiRequest(encodedPrompt);
+
+      message.reply({
+        body: `${geminiResponse}`,
+      }, (err, info) => {
+        global.GoatBot.onReply.set(info.messageID, {
+          commandName: this.config.name,
+          messageID: info.messageID,
+          author: uid,
+          prompt: encodedPrompt,
+          response: geminiResponse,
+        });
+      });
+    } catch (error) {
+      console.error("Error handling command:", error.message);
+    }
+  },
+
+  onStart: function (params) {
+    return this.handleCommand(params);
+  },
+
+  onReply: function (params) {
+    (async () => {
+      try {
+        const uid = params.event.senderID;
+        const context = global.GoatBot.onReply.get(params.messageID);
+
+        if (context) {
+          const { prompt, response } = context;
+          const newPrompt = `${prompt} ${params.args.join(" ")}`;
+
+          const geminiResponse = await this.makeGeminiApiRequest(newPrompt);
+          global.GoatBot.onReply.set(params.messageID, {
+            ...context,
+            prompt: newPrompt,
+            response: geminiResponse,
+          });
+
+          params.message.reply({
+            body: `${geminiResponse}`,
+          });
+        } else {
+          this.handleCommand(params);
+        }
+      } catch (error) {
+        console.error("Error handling reply:", error.message);
+      }
+    })();
+  },
+};
+
+chatbot.onStart();
+
+module.exports = chatbot;
