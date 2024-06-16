a9a03eb5368c50470376299bdbd904cf9ad17677
MarvinWariors <165644652+MarvinWariors@users.noreply.github.com>
2024-06-17 03:13:26 +0800

    Create meina.js

---
+++ scripts/cmds/meina.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
+
+module.exports = {
+  config: {
+    name: "meina",
+    aliases: ["mh"],
+    version: "1.0",
+    author: "vex_Kshitiz",
+    countDown: 5,
+    role: 0,
+    shortDescription: "meina hent*i",
+    longDescription: "create NSFW illustrations ",
+    category: "image",
+    guide: {
+      en: "{p}meina [prompt]"
+    }
+  },
+  onStart: async function ({ message, event, args, api }) {
+    api.setMessageReaction("🕐", event.messageID, (err) => {}, true);
+    try {
+      const baseUrl = "https://kshitiz-t2i-x6te.onrender.com/sdxl";
+      let prompt = '';
+      const model_id = 8; 
+
+      if (args.length > 0) {
+        prompt = args.join(" ").trim();
+      } else {
+        return message.reply("❌ | Please provide a prompt.");
+      }
+
+      const apiResponse = await axios.get(baseUrl, {
+        params: {
+          prompt: prompt,
+          model_id: model_id
+        }
+      });
+
+      if (apiResponse.data.imageUrl) {
+        const imageUrl = apiResponse.data.imageUrl;
+        const imagePath = path.join(__dirname, "cache", `${Date.now()}_generated_image.png`);
+        const imageResponse = await axios.get(imageUrl, { responseType: "stream" });
+        const imageStream = imageResponse.data.pipe(fs.createWriteStream(imagePath));
+        imageStream.on("finish", () => {
+          const stream = fs.createReadStream(imagePath);
+          message.reply({
+            body: "",
+            attachment: stream
+          });
+        });
+      } else {
+        throw new Error("Image URL not found in response");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      message.reply("❌ | An error occurred. Please try again later.");
+    }
+  }
+};
